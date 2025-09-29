9c1b79d27e65d6d9e37bcdf8b5766264c79daea7
Rashel124 <webcoderitrashel124@gmail.com>
2025-09-29 04:26:35 -0700

    update

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,43 +11,8 @@ class Product extends Model
 
     protected $guarded = [];
 
-<<<<<<< HEAD
-=======
-    public function category()
-    {
-        return $this->belongsTo(Category::class, 'cat_id', 'id');
-    }
 
-    public function subCategory()
-    {
-        return $this->belongsTo(SubCategory::class, 'sub_cat_id', 'id');
-    }
-
-    public function color()
-    {
-        return $this->hasMany(Color::class, 'product_id', 'id');
-    }
-
-    public function size()
-    {
-        return $this->hasMany(Size::class, 'product_id', 'id');
-    }
-
-    public function galleryImage()
-    {
-        return $this->hasMany(GalleryImage::class, 'product_id', 'id');
-    }
-
-    public function cart()
-    {
-        return $this->hasMany(Cart::class, 'product_id', 'id');
-    }
-
-    public function orderDetails()
-    {
-        return $this->hasMany(OrderDetails::class, 'product_id', 'id');
-    }
->>>>>>> 82d45725992da4c6ed1b302ecae07eb8a760b52b
 }
+
 
 
